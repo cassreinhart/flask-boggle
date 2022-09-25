3485a68bc4399ec4dee49d59880663814c40e50c
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2022-09-25 14:34:53 -0500

    added unittests

--- static/app.js
+++ static/app.js
@@ -1,43 +1,78 @@
 const $form = $(".add-word");
 const $timer = $(".timer");
+let time;
+let score;
+let words;
+
+startGame();
 
 function handleSubmit(evt) {
     evt.preventDefault();
     button = evt.target;
     let word = $('.word').val()
     
-    post_word(word);
-    
+    $('.word').val('');
+
+    if (!words.has(word)){
+        words.add(word);
+        post_word(word);
+    } else $(".messages").text(`Word "${word}" already played.`)
 }
 
+function startGame() {
+    $('.new-game').hide();
+    $('.add-word').show();
+    time = 60;
+    score = 0;
+    words = new Set();
+}
+
+function endGame(){
+    $(".add-word").hide();
+    $('.messages').text(`Your Score: ${score}`);
+    $('.new-game').show();
+
+    post_score(score);
+}
+
+const timer = setInterval(function() {
+    time -= 1;
+    
+    $('.timer').text(time + 's');
+    if (time < 0) {
+        $('.timer').text('TIME UP!!!')
+        clearInterval(1);
+        endGame();
+    }
+}, 1000);
+
 async function post_word(word) {
-    $('.messages').innerText('');
+    $('.messages').text('');
 
     let response = await axios.post("/check-word", {word});
-    console.log('DONE!', response)
 
-    $('.messages').innerText(response.data.result); //is there a better way?
+    $('.messages').text(response.data.result);
     if (response.data.result === 'ok') {
-        let score = word.length;
-        post_score(score);
-        $('.list').append(word);
-        
+        score += word.length;
+
+        $('ul').append(`<li>${word}</li>`);
     }
-    // timer();
 }
 
 $form.on('submit', handleSubmit);
 
+$('.new-game').on('click', startGame)
+
 async function post_score(score) {
     
     let response = await axios.post('/update-score', {score}); 
-    console.log('done', response)
+    // console.log('done', response)
 
 }
 
 // class BoggleGame {
-//     constructor(boardId, seconds = 60) {
-//         this.seconds = seconds;
+//     constructor(boardId, time = 60) {
+//         this.time = time;
 //         this.showTimer();
 //         this.timer = setInterval(this.tick.bind(this), 1000);
 //         this.score = 0;
@@ -47,7 +82,59 @@ async function post_score(score) {
 //         $(".add-word", this.board).on('submit', this.handleSubmit.bind(this));
 //     }
 
-//     showPlayedWord(word) {
-//         $
+//     handleSubmit(evt) {
+//         evt.preventDefault();
+//         button = evt.target;
+//         let word = $('.word').val()
+        
+//         this.post_word(word);
+//     }
+//     startGame() {
+//         $('.new-game').hide();
+//         $('.add-word').show();
+//         this.time = 60;
+//         this.score = 0;
+//     }
+    
+//     endGame(){
+//         $(".add-word").hide();
+//         $('.messages').text(`Your Score: ${score}`);
+//         $('.new-game').show();
+    
+//         this.post_score(score);
+//     }
+    
+//     timer = setInterval(function() {
+//         this.time -= 1;
+        
+//         $('.timer').text(this.time + 's');
+//         if (this.time < 0) {
+//             $('.timer').text('TIME UP!!!')
+//             this.endGame();
+//         }
+//     }, 1000);
+    
+//     async post_word(word) {
+//         $('.messages').text('');
+    
+//         let response = await axios.post("/check-word", {word});
+    
+//         $('.messages').text(response.data.result);
+//         if (response.data.result === 'ok') {
+//             score += word.length;
+    
+//             $('ul').append(`<li>${word}</li>`);
+//         }
+//     }
+    
+//     //////////////////how to use event listeners in a class?
+//     // $form.on('submit', handleSubmit);
+    
+//     // $('.new-game').on('click', startGame)
+    
+//     async post_score(score) {
+        
+//         let response = await axios.post('/update-score', {score}); 
+//         // console.log('done', response) //////////////////should this function do more?
 //     }
 // }
--- tests/test.py
+++ tests/test.py
@@ -6,5 +6,44 @@ from boggle import Boggle
 
 class FlaskTests(TestCase):
 
-    # TODO -- write tests for every view function / feature!
+    def test_board(self):
+        with app.text_client() as client:
+            res = client.get('/')
+            html = res.get_data(as_text=True)
+            self.assertEqual(res.status_code, 200)
+            self.assertIn('<table class="main">')
+            self.assertIsNone(session['highscore'])
+            self.assertIsNone(session['num_plays'])
+    
+    def test_session_num_plays(self):
+        with app.test_client() as client:
+            with client.session_transaction() as change_session:
+                change_session['num_plays']= 999
+            
+            res = client.get('/update-score')
 
+            self.assertEqual(res.status_code, 200)
+            self.assertEqual(session['num_plays'], 1000)
+
+    def test_valid_word(self):
+        with app.test_client() as client:
+            with client.session_transaction() as sess:
+                sess['board'] = [["B","A","R","R","R"],
+                                ["B","A","R","R","R"],
+                                ["B","A","R","R","R"],
+                                ["B","A","R","R","R"],
+                                ["B","A","R","R","R"]]
+                response = self.client.get('/check-word?word=bar')
+                self.assertEqual(response.json['result'], 'ok')
+
+        def test_invalid_word(self):
+            with app.test_client() as client:
+                self.client.get('/')
+                response = self.client.get('/check-word?word=fool')
+                self.assertEqual(response.json['result'], 'not-on-board')
+
+        def test_not_a_word(self):
+            with app.test_client() as client:
+                self.client.get('/')
+                response = self.client.get('/check-word?word=awvrvwai')
+                self.assertEqual(response.json['result'], 'not-word')
